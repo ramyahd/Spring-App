fdad5f86457221d749c1378d2d1fe44cb732ee9b
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-07-01 12:28:10 +0530

    Update SpringbootAssignmentApplication.java

--- src/main/java/com/example/demo/SpringbootAssignmentApplication.java
+++ src/main/java/com/example/demo/SpringbootAssignmentApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringbootAssignmentApplication {
+public class SpringbootAssignmentApplication extends ServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAssignmentApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(SpringbootAssignmentApplication.class);
 
 }
